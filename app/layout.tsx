46acc13665c85abf1a143f60278361afd655ca47
Naresh <ghostwriternr@gmail.com>
2023-06-15 12:44:06 +0530

    Add OG images

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,13 @@ import { Providers } from "./providers";
 const font = Space_Mono({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
-  title: "CodeStory",
-  description: "Plan. Debug. Reason.",
+  title: "CodeStory: Plan. Debug. Reason. Within your IDE.",
+  description:
+    "CodeStory is an AI coding assistant that learns the context of your code and helps you plan, debug and reason with code on a day-to-day basis.",
+  applicationName: "CodeStory",
+  referrer: "origin-when-cross-origin",
+  keywords: ["CodeStory", "YC", "YCombinator", "Developer Tools", "AI"],
+  colorScheme: "dark",
 };
 
 export default function RootLayout({
