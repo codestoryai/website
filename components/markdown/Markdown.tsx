dfb34ad1ee83878eaebda5c00effe8ef39476219
Naresh <ghostwriternr@gmail.com>
2023-08-19 06:52:45 -0700

    Use NextJS image component to load images on demand

--- components/markdown/Markdown.tsx
+++ components/markdown/Markdown.tsx
@@ -1,6 +1,59 @@
+import Image from "next/image"
 import ReactMarkdown from "react-markdown"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { coldarkDark as theme } from "react-syntax-highlighter/dist/cjs/styles/prism"
+
+const markdownComponents: object = {
+	p(paragraph: { children?: boolean; node?: any }) {
+		const { node } = paragraph
+
+		if (node.children[0].tagName === "img") {
+			const image = node.children[0]
+			const blurSupported = image.properties.src.split(".").pop() in ["jpg", "png", "webp", "avif"]
+			const metastring = image.properties.alt
+			const alt = metastring?.replace(/ *\{[^)]*\} */g, "")
+			const metaWidth = metastring.match(/{([^}]+)x/)
+			const metaHeight = metastring.match(/x([^}]+)}/)
+			const width = metaWidth ? metaWidth[1] : "768"
+			const height = metaHeight ? metaHeight[1] : "432"
+			const isPriority = metastring?.toLowerCase().match('{priority}')
+			const hasCaption = metastring?.toLowerCase().includes('{caption:')
+			const caption = metastring?.match(/{caption: (.*?)}/)?.pop()
+
+			return (
+				<div className="postImgWrapper">
+					<Image
+						src={image.properties.src}
+						width={width}
+						height={height}
+						className="postImg"
+						alt={alt}
+						priority={isPriority}
+						placeholder={blurSupported ? "blur" : "empty"}
+					/>
+					{hasCaption ? <div className="caption" aria-label={caption}>{caption}</div> : null}
+				</div>
+			)
+		}
+		return <p>{paragraph.children}</p>
+	},
+	code({ node, inline, className, children, ...props }) {
+		const match = /language-(\w+)/.exec(className || '')
+		return !inline && match ? (
+			<SyntaxHighlighter
+				{...props}
+				children={String(children).replace(/\n$/, '')}
+				style={theme}
+				language={match[1]}
+				PreTag="div"
+			/>
+		) : (
+			<code {...props} className={className}>
+				{children}
+			</code>
+		)
+	}
+}
 
 type MarkdownProps = {
 	content: string;
@@ -10,24 +63,7 @@ export const Markdown = ({ content }: MarkdownProps) => {
 	return (
 		<ReactMarkdown
 			children={content}
-			components={{
-				code({ node, inline, className, children, ...props }) {
-					const match = /language-(\w+)/.exec(className || '')
-					return !inline && match ? (
-						<SyntaxHighlighter
-							{...props}
-							children={String(children).replace(/\n$/, '')}
-							style={theme}
-							language={match[1]}
-							PreTag="div"
-						/>
-					) : (
-						<code {...props} className={className}>
-							{children}
-						</code>
-					)
-				}
-			}}
+			components={markdownComponents}
 		/>
 	)
 }
