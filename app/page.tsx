eb47cb917efef0675be64a526be04fa7b0c3b813
Naresh <ghostwriternr@gmail.com>
2024-03-14 03:46:19 +0000

    Add more sections to the header

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Download } from "lucide-react";
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
-import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle, NavigationMenuTrigger, NavigationMenuContent, ListItem } from "@/components/ui/navigation-menu";
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
 
 import AideDesktopSS from "../public/aide-desktop.png";
@@ -29,19 +29,95 @@ export default function Home() {
           <NavigationMenu className="cursor-pointer">
             <NavigationMenuList>
               <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>About</NavigationMenuLink>
+                <NavigationMenuTrigger>About</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    <li className="row-span-3">
+                      <NavigationMenuLink asChild>
+                        <a
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-2 no-underline outline-none focus:shadow-md"
+                          href="https://codestory.ai"
+                        >
+                          <Image
+                            src="/cs-logomark.svg"
+                            alt="CodeStory Logo"
+                            width={48}
+                            height={48}
+                            priority
+                          />
+                          <div className="m-2 text-lg font-semibold">
+                            CodeStory
+                          </div>
+                          <p className="m-2 mt-0 text-sm leading-tight text-muted-foreground">
+                            Get to know the company behind Aide.
+                          </p>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                    <ListItem href="/docs/installation" title="Manifesto">
+                      What we stand for, and why it matters.
+                    </ListItem>
+                    <ListItem href="/docs/installation" title="Team">
+                      Aide is built by a team of two based in London, United Kingdom.
+                    </ListItem>
+                    <ListItem href="/docs/primitives/typography" title="Careers">
+                      Work with us to re-imagine software engineering for the coming decade.
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Docs</NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Releases</NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Blog</NavigationMenuLink>
+                <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    <ListItem
+                      title="Docs"
+                      href="/docs"
+                    >
+                      Latest documentation for Aide.
+                    </ListItem>
+                    <ListItem
+                      title="Releases"
+                      href="/releases"
+                    >
+                      Stay up-to-date on new changes to Aide.
+                    </ListItem>
+                    <ListItem
+                      title="Blog"
+                      href="/blog"
+                    >
+                      Read about our thoughts and challenges behind building Aide.
+                    </ListItem>
+                    <ListItem
+                      title="Open Source"
+                      href="/open-source"
+                    >
+                      Take a look under the hood at some of our Open Source components.
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>Pricing</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Enterprise</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid grid-cols-1 w-[200px] gap-3 p-4 lg:w-[300px]">
+                    <ListItem
+                      title="Overview"
+                      href="/"
+                    >
+                      Understand Aide&apos;s Enterprise offering.
+                    </ListItem>
+                    <ListItem
+                      title="Contact Sales"
+                      href="/"
+                    >
+                      Want to learn more, or ready to onboard? Speak with us!
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
                 <NavigationMenuLink className={`${navigationMenuTriggerStyle()} font-semibold`}>Download</NavigationMenuLink>
@@ -56,7 +132,7 @@ export default function Home() {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <span className="underline decoration-2">Aide</span>
+                <span className="underline underline-offset-8 decoration-2">Aide</span>
               </TooltipTrigger>
               <TooltipContent side="bottom">
                 <p className="text-xl">AI + IDE</p>
