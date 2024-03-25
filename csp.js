071e62cebe3232b7d85c5d9103ed0d15dfe98427
Naresh <ghostwriternr@gmail.com>
2024-03-25 06:08:48 +0000

    Whitelist jsdeliver.net in CSP for the monaco editor

--- csp.js
+++ csp.js
@@ -5,7 +5,7 @@ const policies = {
   'font-src': ["'self'"],
   'frame-src': ["'self'"],
   'img-src': ["'self'", 'https://raw.githubusercontent.com'],
-  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com'],
+  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://cdn.jsdelivr.net'],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
 }
 
