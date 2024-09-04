bb43517da9ba94e75869de0fdb648b8341709304
Naresh <ghostwriternr@gmail.com>
2024-09-04 19:53:14 +0100

    Handle free accounts properly

--- types/api.ts
+++ types/api.ts
@@ -18,7 +18,7 @@ export type UpcomingInvoice = {
 
 export const SubscriptionStatuses: Record<SubscriptionStatus, string> = {
     free: "Free tier",
-    active: "Active",
+    active: "Pro",
     cancelled: "Cancelled",
 };
 
